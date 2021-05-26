e3a699cedb40ea61c5ff85a8dc3fedc61878e3aa
Rakesh <rakeshgupth91@gmail.com>
2021-05-26 14:09:07 +0530

    New file is created for action and webdriver wait.

--- SeleniumBasics/workingwithactionsandwait.py
+++ SeleniumBasics/workingwithactionsandwait.py
@@ -17,18 +17,31 @@ class Actionwait():
     action=ActionChains(driver)
     currentwindow=driver.current_window_handle
     driver.switch_to_window(currentwindow)
-    #driver.implicitly_wait(10)
-    wait.until(EC.NoSuchElementException, (By.CSS_SELECTOR, 'a[data-ga-track="Main Navigation Jobs|Jobs Icon"]'))
-    jobscss =driver.find_element_by_css_selector('a[data-ga-track="Main Navigation Jobs|Jobs Icon"]')
-    jobsmove=action.move_to_element(jobscss)
-    jobsmove.perform()
-    time.sleep(5)
-    wait.until(EC.NoSuchElementException,(By.CSS_SELECTOR,'a[data-ga-track="Main Navigation Recruiters|Recruiters Icon"]'))
-    recuterscss=driver.find_element(By.CSS_SELECTOR,'a[data-ga-track="Main Navigation Recruiters|Recruiters Icon"]')
-    recutersmove=action.move_to_element(recuterscss).perform()
-    time.sleep(10)
-    alllinksunderrecuter=driver.find_elements_by_xpath('//ul[@class="midSec menu"]/li[2]/div[@class="subMenu"]/ul/li')
-    print(alllinksunderrecuter)
+    driver.implicitly_wait(10)
+    # wait.until(EC.NoSuchElementException, (By.CSS_SELECTOR, 'a[data-ga-track="Main Navigation Jobs|Jobs Icon"]'))
+    # jobscss =driver.find_element_by_css_selector('a[data-ga-track="Main Navigation Jobs|Jobs Icon"]')
+    # jobsmove=action.move_to_element(jobscss)
+    # jobsmove.perform()
+    # time.sleep(3)
+    # wait.until(EC.NoSuchElementException,(By.CSS_SELECTOR,'a[data-ga-track="Main Navigation Recruiters|Recruiters Icon"]'))
+    # recuterscss=driver.find_element(By.CSS_SELECTOR,'a[data-ga-track="Main Navigation Recruiters|Recruiters Icon"]')
+    # recutersmove=action.move_to_element(recuterscss).perform()
+    time.sleep(3)
+    allheaders=driver.find_elements_by_xpath('//ul[@class="midSec menu"]/li')
+    print(len(allheaders))
+    for i in allheaders:
+        print(i)
+        moveheader=action.move_to_element(i).perform()
+        time.sleep(2)
+        continue
+        print(i.get_attribute('innerText'))
+
+    # alllinksunderrecuter=driver.find_elements_by_xpath('//ul[@class="midSec menu"]/li[2]/div[@class="subMenu"]/ul/li')
+    # print(len(alllinksunderrecuter))
+    # for i in alllinksunderrecuter:
+    #     moverecuters=action.move_to_element(i).perform()
+    #     print(i.get_attribute('innerText'))
+    #     time.sleep(2)
 
     driver.quit()
 
